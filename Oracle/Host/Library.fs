6597b4fca8fbdc198aa562a87bc1d1e5e342296e
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2022-02-17 08:57:33 -0300

    feat: ⚗️ more tests

---
+++ Oracle/Host/Library.fs
@@ -0,0 +1,29 @@
+﻿namespace Host
+
+open System.Diagnostics
+open Quantum
+open Microsoft.Quantum.Simulation.Simulators
+open Microsoft.Quantum.Simulation.Core
+
+module Host = 
+    [<EntryPoint>]
+    let main _ =
+        printfn "Hello, from classical world"
+
+        use simulator = new QuantumSimulator();
+
+        let parity = Seq.reduce(<>)
+
+        let sampleBits = new QArray<bool>([| false; true; false |]);
+        printfn "Samples bits: %A" sampleBits
+
+        let algorithm = RunAlgorithm.Run(simulator, sampleBits).Result
+        printfn "Bits from Quantum Computation: %A" algorithm
+
+        // Process the results: in this case, verify that:
+        // - the length of the return array equals the length of the input array
+        Debug.Assert(algorithm.Length = sampleBits.Length, "Return array length differs from the input array length")
+        // - the parity of the returned array matches the parity of the input one
+        Debug.Assert((algorithm |> parity) = (sampleBits |> parity), "Return array should have the same parity as the input one")
+
+        0
---
+++ Oracle/Quantum/Library.qs
@@ -0,0 +1,15 @@
+﻿namespace Quantum {
+    
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;
+    
+    /// Instantiates the oracle and runs the parameter restoration algorithm.
+    operation RunAlgorithm(bits : Bool[]) : Bool[] {
+        Message("Hello, quantum world!");
+        return [false, size = 10];
+        // construct an oracle using the input array
+        //let oracle = ApplyProductWithNegationFunction(bits, _, _);
+        // run the algorithm on this oracle and return the result
+        //return ReconstructOracleParameters(Length(bits), oracle);
+    }
+}
